04cf93699ba59a170d4b17f3341ef5aa093f6e95
tienpmath <designdalat@gmail.com>
2024-11-22 23:01:56 +0700

    seo

--- src/app/(home)/layout.tsx
+++ src/app/(home)/layout.tsx
@@ -5,12 +5,12 @@ export const metadata = {
   title:
     'Cho thuê xe du lịch Đà Lạt giá rẻ xe đời mới - Du Lịch Trung Thành ĐÀ LẠT',
   description:
-    'Cho thuê xe du lịch Đà Lạt 4 5 7 9 16 24 29 chỗ giá rẻ xe đời mới, uy tín tại Đà Lạt, Bảo Lộc.☎️ 0918 638 068 Du Lịch Trung Thành ĐÀ LẠT thuê ô tô xe đời mới✔️✔️ giao xe ngay',
+    'Cho thuê xe du lịch Đà Lạt 4 5 7 9 16 24 29 chỗ giá rẻ xe đời mới, uy tín tại Đà Lạt.☎️ 0918 638 068 Du Lịch Trung Thành ĐÀ LẠT thuê ô tô xe đời mới✔️✔️ giao xe ngay',
   openGraph: {
     title:
       'Cho thuê xe du lịch Đà Lạt giá rẻ xe đời mới - Du Lịch Trung Thành ĐÀ LẠT',
     description:
-      'Cho thuê xe du lịch Đà Lạt 4 5 7 9 16 24 29 chỗ giá rẻ xe đời mới, uy tín tại Đà Lạt, Bảo Lộc.☎️ 0918 638 068 Du Lịch Trung Thành ĐÀ LẠT thuê ô tô xe đời mới✔️✔️ giao xe ngay',
+      'Cho thuê xe du lịch Đà Lạt 4 5 7 9 16 24 29 chỗ giá rẻ xe đời mới, uy tín tại Đà Lạt.☎️ 0918 638 068 Du Lịch Trung Thành ĐÀ LẠT thuê ô tô xe đời mới✔️✔️ giao xe ngay',
     images: '/logo.png',
   },
 };
